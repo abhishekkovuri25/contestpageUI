171623144cf1e09000395540a1949aef92901546
kovuriabhishek <abhishek.kovuri@coviam.com>
2019-01-29 09:26:28 +0530

    checkbox

---
+++ src/components/checkbox.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <div v-for="item in parks" :key="item">
+      <input type="checkbox" :value="item" v-model="selectedParks"/>
+        <label> {{item.question}}</label>
+    </div>
+<span>Checked: {{selectedParks}}</span>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+    parks:[
+      {
+        question:"who is the pm?"
+      },
+      {
+        question:"who is president?"
+      },
+      {
+        question:"who is gandhi?"
+      },
+      {
+        question:"who are you?"
+      }
+    ],
+    selectedParks:[]
+  }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import checkbox from '@/components/checkbox'
 Vue.use(Router)
+Vue.use(BootstrapVue)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/cb',
+      name: 'checkbox',
+      component:checkbox
     }
   ]
 })
